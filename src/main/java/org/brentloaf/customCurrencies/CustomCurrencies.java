e7c167637c16f76c83727ac3a38f0abd2b8352e4
BrentLoaf <kcoldoowtnerbg@gmail.com>
2026-02-18 15:31:55 -0800

    Initial commit

---
+++ src/main/java/org/brentloaf/customCurrencies/CustomCurrencies.java
@@ -0,0 +1,17 @@
+package org.brentloaf.customCurrencies;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CustomCurrencies extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
