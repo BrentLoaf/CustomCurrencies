062a5e52af50ec8d71edce99e6f62548446115cb
BrentLoaf <kcoldoowtnerbg@gmail.com>
2026-03-10 23:53:06 -0700

    Added an account table for the currencies of players.

--- src/main/java/org/brentloaf/customCurrencies/account/Account.java
+++ src/main/java/org/brentloaf/customCurrencies/account/Account.java
@@ -1,7 +1,6 @@
 package org.brentloaf.customCurrencies.account;
 
 import org.brentloaf.customCurrencies.currency.Currency;
-import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
 import java.util.HashMap;
--- src/main/java/org/brentloaf/customCurrencies/database/Database.java
+++ src/main/java/org/brentloaf/customCurrencies/database/Database.java
@@ -1,8 +1,26 @@
 package org.brentloaf.customCurrencies.database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Database {
 
-    public Database() {
+    private final Connection connection;
 
+    public Database(String path) throws SQLException {
+        connection = DriverManager.getConnection("jdbc:sqlite:" + path);
+
+        try (Statement statement = connection.createStatement()) {
+            statement.execute("""
+                CREATE TABLE IF NOT EXISTS accountTable (
+                    ownerUuid TEXT NOT NULL,
+                    currency TEXT NOT NULL,
+                    balance INT NOT NULL,
+                    PRIMARY KEY (ownerUuid, currency)
+                )
+            """);
+        }
     }
 }
